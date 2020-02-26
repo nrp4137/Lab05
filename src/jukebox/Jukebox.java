fc0876b4657f1c13b9b763ec7f4c6adf03a73e92
Nolan Porter <nrp4137@rit.edu>
2020-02-25 23:08:58 -0500

    Did all of the functions mostly done

--- src/jukebox/Jukebox.java
+++ src/jukebox/Jukebox.java
@@ -4,42 +4,90 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+/** Nolan Porter
+ * Class that represents a jukebox with songs in it
+ */
 public class Jukebox {
 
     private HashMap<Song, Integer> song_list;
-    private Random seed;
+    private int total;
+    private int size;
+    private int average;
+    private long time;
+
 
     public static void main(String[] args) throws FileNotFoundException {
+        Jukebox juke = new Jukebox(args[0]);
+        String param1 = args[1];
+        int param = Integer.parseInt(param1);
+        juke.run_simulation(param);
+        juke.generate_stats();
+        juke.print_stats();
+
 
     }
 
+    /**
+     * Constructor for jukebox. It loads in a list of songs from a given file into a hash map
+     * @param filename the file given of the command line
+     * @throws FileNotFoundException
+     */
     public Jukebox(String filename) throws FileNotFoundException {
         Scanner in = new Scanner(new File(filename));
+        HashMap<Song, Integer> map = new HashMap<>();
         while (in.hasNextLine()) {
             String line = in.nextLine();
             String[] split_line = line.split("<SEP>", 4);
             String artist = split_line[2];
             String song = split_line[3];
             Song next_song = new Song(artist, song);
-            if (!(this.song_list.containsKey(next_song))) {
-                this.song_list.put(next_song, 0);
-            }
+
+            map.put(next_song, 0);
+
+
         }
         in.close();
+        this.song_list = map;
     }
 
-    public void run_simulation(long seed1) {
-        this.seed = new Random(seed1);
+    public void run_simulation(int seed1) {
+        Random rnd = new Random(seed1);
         int i;
         int counter = 0;
 
-        Set<Song> new_list = song_list.keySet();
-        HashSet<Song> already_played = new HashSet<>();
+        Set<Song> new_set = song_list.keySet();
+        
+        long start_time = System.currentTimeMillis();
         for (i = 0; i < 50000; i++) {
-            while()
-
-
+            HashSet<Song> already_played = new HashSet<>();
+            Song[] new_list = new_set.toArray(new Song[0]);
+            int indexer = rnd.nextInt(new_set.size());
+            while(! already_played.contains(new_list[indexer])) {
+                counter += 1;
+                already_played.add(new_list[indexer]);
+                int value = song_list.get(new_list[indexer]) + 1;
+                song_list.put(new_list[indexer], value);
+                indexer = rnd.nextInt(new_set.size());
+            }
         }
+        long end_time = System.currentTimeMillis();
+        this.total = counter;
+        this.time = (end_time - start_time)/ 1000;
+
+
+    }
+
+    public void generate_stats() {
+        this.average = total / 50000;
+        this.size = song_list.size();
+
+    }
+
+    public void print_stats() {
+        System.out.println("Jukebox of " + size + " songs");
+        System.out.println("Simulation took " + time + " seconds");
+        System.out.println("Total number of songs played " + total);
+        System.out.println("Average number of songs played " + average);
 
     }
 }
--- src/jukebox/Song.java
+++ src/jukebox/Song.java
@@ -1,23 +1,46 @@
 package jukebox;
 
+
+/** Nolan Porter
+ * Class that represents songs to be put in the jukebox
+ */
 public class Song implements Comparable <Song> {
 
     private String artist;
     private String song;
 
+
+    /**
+     * Constructor for Song initializes fields
+     * @param artist artist of song as a string
+     * @param song song title as string
+     */
     public Song(String artist, String song) {
         this.artist = artist;
         this.song = song;
     }
 
+    /**
+     * gets the artist name from the field
+     * @return String
+     */
     public String getArtist() {
         return this.artist;
     }
 
+    /**
+     * gets the song name from field
+     * @return String
+     */
     public String getSong() {
         return this.song;
     }
 
+    /**
+     * overrides the equals method to show whether two songs are the same
+     * @param o object
+     * @return a boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Song)) {
@@ -30,11 +53,20 @@ public class Song implements Comparable <Song> {
         return false;
     }
 
+    /**
+     * creates a unique number for different instances of songs, overriding hashCode default method
+     * @return int
+     */
     @Override
     public int hashCode() {
         return song.hashCode() + artist.hashCode();
     }
 
+    /**
+     * Compares two songs and orders them in alphabetical by artist
+     * @param other Song
+     * @return int
+     */
     public int compareTo(Song other) {
         int artistC = this.artist.compareTo(other.artist);
 
